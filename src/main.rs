a2e8850d525256df2e5af4ad4cc1edcd3ec5fec6
dunce <sa78rt@outlook.com>
2021-03-22 20:18:56 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(||{
+			handle_connection(stream)
+		});
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+	let response = "HTTP/1.1 200 OK\r\n\r\n";
+	stream.write(response.as_bytes()).unwrap();
+	stream.flush().unwrap();
+}
